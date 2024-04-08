f19470e17bbb5cc6766cf5b1df70f7428bf711f7
asscent <zhdanov6d@mail.ru>
2024-04-08 14:37:58 +0700

    +сам щит

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template("base_text_info.html", html_stat=open(r'C:\Users\User\Desktop\Новая папка (5)\f4ckschool1\templates\ГЛАВА 1\Что такое инетрнет.html', encoding='utf-8').read())
+    return render_template("base_text_info.html", html_stat=open(r'templates\ГЛАВА 1\Что такое инетрнет.html', encoding='utf-8').read())
 
 if __name__ == '__main__':
     app.run(debug=True)
